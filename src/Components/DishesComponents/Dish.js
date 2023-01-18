978491cde5efce92502b0a07a35b0c4d75e13ddf
kateFrontend <ecdanilova@gmail.com>
2023-01-18 17:03:03 -0500

    setup images

--- src/Components/DishesComponents/Dish.js
+++ src/Components/DishesComponents/Dish.js
@@ -3,6 +3,7 @@ import React from "react";
 function Dish({ dish }) {
   return (
     <div>
+        <img src={`${dish.img}.jpg`}/>
       <h2>{dish.name}</h2>
       <p>$ {dish.price}</p>
     </div>
