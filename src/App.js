1fe04e1020e06938a80586b9dd43b30b6f7002c8
kateFrontend <ecdanilova@gmail.com>
2023-01-18 16:35:27 -0500

    update css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import './App.css'
-import Cart from './Components/Cart/Cart'
-import Dishes from './Components/DishesComponents/Dishes'
-import AllCategories from './Components/Filter/AllCategories'
+import "./App.css";
+import Cart from "./Components/Cart/Cart";
+import Dishes from "./Components/DishesComponents/Dishes";
+import AllCategories from "./Components/Filter/AllCategories";
 
 function App() {
   return (
-    <div className='App'>
-      <Dishes/>
-      <AllCategories/>
-      <Cart/>
+    <div className="App">
+      <div className="block">
+        <Dishes />
+        <AllCategories />
+      </div>
+      <div className="block">
+      <Cart />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
