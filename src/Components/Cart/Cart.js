dc80c68890ae163e784a1eb9794a42ecafaba20a
kateFrontend <ecdanilova@gmail.com>
2023-01-19 17:07:10 -0500

    connect store & CartItem

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
+import { getcartItems } from '../../redux/cartSlice';
+import CartItem from './CartItem'
 
 function Cart() {
+
+  const cartItems = useSelector(getcartItems)
+
   return (
     <div>
       <img className="cartIcon" src="https://img.icons8.com/external-xnimrodx-lineal-color-xnimrodx/64/000000/external-shopping-cart-cyber-monday-xnimrodx-lineal-color-xnimrodx.png"/> 
+      {cartItems.map(cartItem => <CartItem cartItem={cartItem}/>)}
+      <CartItem/>
     </div>
   )
 }
---
+++ src/Components/Cart/CartItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CartItem({cartItem}) {
+  return <div>cartItem</div>;
+}
+
+export default CartItem;
