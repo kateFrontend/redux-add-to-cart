8a40311a588ee28979ffdeb96c020fd1f6354ddc
kateFrontend <ecdanilova@gmail.com>
2023-01-19 13:20:58 -0500

    redux components

---
+++ src/redux/dishesSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const dishesSlice = createSlice({
+    name: 'dishes',
+    initialState: {
+        selectedCategory: "SEAFOOD"
+    },
+    reducers: {
+
+    }
+})
+
+export const getSelectedCategory = state => state.dishes.selectedCategory;
+
+export default dishesSlice.reducer;
