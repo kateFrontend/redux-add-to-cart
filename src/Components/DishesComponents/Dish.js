523dd56050921f030033e68b6c1c935d70c52bee
kateFrontend <ecdanilova@gmail.com>
2023-01-22 13:41:16 -0500

    connect components

--- src/Components/DishesComponents/Dish.js
+++ src/Components/DishesComponents/Dish.js
@@ -1,15 +1,25 @@
 import { useState } from "react";
 import ChangeQuantity from "../Cart/ChangeQuantity";
+import { useDispatch } from "react-redux";
+import { addItemToCart } from '../../redux/cartSlice'
 
 function Dish({ dish }) {
   const [quantity, setQuantity] = useState(1);
+  const dispatch = useDispatch()
+
   return (
     <div>
-        <img src={`${dish.img}.jpg`}/>
+      <img src={`${dish.img}.jpg`} />
       <h2>{dish.name}</h2>
       <p>$ {dish.price}</p>
-      <ChangeQuantity quantity={quantity} setQuantity={setQuantity}/>
-      <button>Add to cart</button>
+      <ChangeQuantity quantity={quantity} setQuantity={setQuantity} />
+      <button
+        onClick={() => {
+          dispatch(addItemToCart({ dish, quantity }));
+        }}
+      >
+        Add to cart
+      </button>
     </div>
   );
 }
