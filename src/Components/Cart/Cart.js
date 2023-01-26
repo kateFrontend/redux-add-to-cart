5cb088d762dee7fa708393d2d424b3b19ea91a39
kateFrontend <ecdanilova@gmail.com>
2023-01-26 13:08:32 -0500

    add total price to cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,15 +1,17 @@
 import { useSelector } from 'react-redux';
-import { getCartItems } from '../../redux/cartSlice';
+import { getCartItems, getTotalPrice } from '../../redux/cartSlice';
 import CartItem from './CartItem'
 
 function Cart() {
 
-  const cartItems = useSelector(getCartItems)
+  const cartItems = useSelector(getCartItems);
+  const totalPrice = useSelector(getTotalPrice);
 
   return (
     <div>
       <img className="cartIcon" src="https://img.icons8.com/external-xnimrodx-lineal-color-xnimrodx/64/000000/external-shopping-cart-cyber-monday-xnimrodx-lineal-color-xnimrodx.png"/> 
       {cartItems.map(cartItem => <CartItem cartItem={cartItem}/>)}
+      <h3>TOTAL: ${totalPrice}</h3>
       <CartItem/>
     </div>
   )
--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -9,11 +9,18 @@ export const slice = createSlice({
     addItemToCart: (state, action) => {
       state.cartItems.push({
         dishId: action.payload.dish.id,
-        quantity: action.payload.quantity
+        quantity: action.payload.quantity,
+        totalPrice: action.payload.quantity * action.payload.dish.price
       });
     },
   },
 });
+
+export const getTotalPrice = state => {
+  return state.cart.cartItems.reduce((total, cartItems) => {
+    return cartItems.totalPrice + total
+  }, 0)
+}
 
 export const getCartItems = state => state.cart.cartItems;
 export const { addItemToCart } = slice.actions;
