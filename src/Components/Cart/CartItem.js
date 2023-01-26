9d42818aaa2057c181ef435735c4c744c70454a4
kateFrontend <ecdanilova@gmail.com>
2023-01-26 13:18:46 -0500

    delete items from cart

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,15 +1,20 @@
 import dataDishes from "../../data/dataDishes";
+import { useDispatch } from "react-redux";
+import { removeItemFromCart } from "../../redux/cartSlice";
 
 const CartItem = ({ cartItem }) => {
   //console.log(cartItem) to check
   const dishes = dataDishes.find((item) => item.id === cartItem.dishId);
   // console.log(dishes) to check
+  const dispatch = useDispatch()
   return (
     <div>
       <p>{dishes.name}</p>
       <p>{cartItem.quantity} portion(s)</p>
       <p>Price: ${dishes.price * cartItem.quantity}</p>
+      <span onClick={() => dispatch(removeItemFromCart({cartItemId: cartItem.id}))}>
       <img className="icon" src="https://img.icons8.com/material-outlined/48/000000/trash--v1.png"/>
+      </span>
     </div>
   );
 };
--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -10,18 +10,23 @@ export const slice = createSlice({
       state.cartItems.push({
         dishId: action.payload.dish.id,
         quantity: action.payload.quantity,
-        totalPrice: action.payload.quantity * action.payload.dish.price
+        totalPrice: action.payload.quantity * action.payload.dish.price,
       });
+    },
+    removeItemFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter(
+        (cartItem) => cartItem.id !== action.payload.cartItemId
+      );
     },
   },
 });
 
-export const getTotalPrice = state => {
+export const getTotalPrice = (state) => {
   return state.cart.cartItems.reduce((total, cartItems) => {
-    return cartItems.totalPrice + total
-  }, 0)
-}
+    return cartItems.totalPrice + total;
+  }, 0);
+};
 
-export const getCartItems = state => state.cart.cartItems;
-export const { addItemToCart } = slice.actions;
+export const getCartItems = (state) => state.cart.cartItems;
+export const { addItemToCart, removeItemFromCart } = slice.actions;
 export default slice.reducer;
