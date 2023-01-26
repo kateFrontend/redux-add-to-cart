b9128cdf71816d478ab854bf989124d3e6771410
kateFrontend <ecdanilova@gmail.com>
2023-01-26 13:10:38 -0500

    add button delete

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -9,6 +9,7 @@ const CartItem = ({ cartItem }) => {
       <p>{dishes.name}</p>
       <p>{cartItem.quantity} portion(s)</p>
       <p>Price: ${dishes.price * cartItem.quantity}</p>
+      <img className="icon" src="https://img.icons8.com/material-outlined/48/000000/trash--v1.png"/>
     </div>
   );
 };
